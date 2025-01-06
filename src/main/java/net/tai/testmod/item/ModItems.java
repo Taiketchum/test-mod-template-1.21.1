2aac76dfe43c657be21cb09e362d1c88d692b28f
TaiKetchum <taiketchum@gmail.com>
2025-01-06 18:45:40 -0400

    items/block update

--- src/main/java/net/tai/testmod/item/ModItems.java
+++ src/main/java/net/tai/testmod/item/ModItems.java
@@ -1,4 +1,30 @@
 package net.tai.testmod.item;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.tai.testmod.TestMod;
+
 public class ModItems {
+    public static final Item RUBY_GEM = registerItem("ruby_gem", new Item(new Item.Settings()));
+    public static final Item RAW_RUBY = registerItem("raw_ruby", new Item(new Item.Settings()));
+    public static final Item DAGGER = registerItem("dagger", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM, Identifier.of(TestMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        TestMod.LOGGER.info("Registering Mod Items for "+ TestMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+           entries.add(RUBY_GEM);
+           entries.add(RAW_RUBY);
+           entries.add(DAGGER);
+
+        });
+    }
 }
